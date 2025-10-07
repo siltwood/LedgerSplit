cb66e7f92bb9959d663977b945c114e65c75a419
Ivar Lee <ivarr5@gmail.com>
2025-10-06 20:55:49 -0700

    Update API baseURL to use api subdomain in production

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.PROD ? 'https://api.ledgersplit.com/api' : '/api',
   withCredentials: true,
 });
 
