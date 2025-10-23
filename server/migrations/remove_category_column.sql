bbee6ca0e90991526c8c6304850c307c52fe77f4
Ivar Lee <ivarr5@gmail.com>
2025-10-23 10:28:14 -0700

    Remove migrations directory

--- server/migrations/remove_category_column.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Remove category column from splits table
--- Run this in Supabase SQL Editor
-
--- Drop the category column from the splits table
-ALTER TABLE splits
-DROP COLUMN IF EXISTS category;
-
--- Add a comment
-COMMENT ON TABLE splits IS 'Splits table - category column removed';
